fded4b120796a671b53cf2b123ccd54b79e9cb1f
wolf2234 <viktoral9599@gmail.com>
2026-01-29 00:37:11 +0300

    this commit adds shadow behiend menu burger.

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,9 +1,11 @@
 const hamMenu = document.querySelector(".burger");
 const offScreenMenu = document.querySelector(".nav-menu");
 const body = document.querySelector("body");
+const header = document.querySelector(".header");
 
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     offScreenMenu.classList.toggle("active");
+    header.classList.toggle("active");
     body.classList.toggle("lock");
 });
