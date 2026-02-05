028c751695e289138e3cc0d290b7bd56157ca0d9
wolf2234 <viktoral9599@gmail.com>
2026-02-05 14:11:55 +0300

    this commit fixes .swiper.

--- woocommerce/single-product/add-to-cart/simple.php
+++ woocommerce/single-product/add-to-cart/simple.php
@@ -69,10 +69,10 @@ if ( $product->is_in_stock() ) : ?>
 			</div>
 			<div class="cart-row">
 				<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
-		
+
 				<?php
 				do_action( 'woocommerce_before_add_to_cart_quantity' );
-		
+
 				woocommerce_quantity_input(
 					array(
 						'min_value'   => $product->get_min_purchase_quantity(),
@@ -80,12 +80,12 @@ if ( $product->is_in_stock() ) : ?>
 						'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
 					)
 				);
-		
+
 				do_action( 'woocommerce_after_add_to_cart_quantity' );
 				?>
-		
+
 				<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-		
+
 				<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 			</div>
 		</div>
