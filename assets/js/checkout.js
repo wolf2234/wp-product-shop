bd4a15e2aa703d8398d85b590475b8febf095e4d
wolf2234 <viktoral9599@gmail.com>
2026-07-04 23:35:59 +0300

    this commit adds styles for success-details__item.

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,13 +1,15 @@
 const stripe = Stripe(stripe_data.publishable_key);
-document.getElementById("pay-btn").addEventListener("click", async () => {
-    const formData = new FormData();
-    formData.append("action", "create_checkout_session");
-    const response = await fetch(product_obj.ajaxurl, {
-        method: "POST",
-        body: formData,
+if (document.getElementById("pay-btn")) {
+    document.getElementById("pay-btn").addEventListener("click", async () => {
+        const formData = new FormData();
+        formData.append("action", "create_checkout_session");
+        const response = await fetch(product_obj.ajaxurl, {
+            method: "POST",
+            body: formData,
+        });
+        const result = await response.json();
+        stripe.redirectToCheckout({
+            sessionId: result.data.session_id,
+        });
     });
-    const result = await response.json();
-    stripe.redirectToCheckout({
-        sessionId: result.data.session_id,
-    });
-});
+}
--- page-success-order.php
+++ page-success-order.php
@@ -47,35 +47,60 @@ if (!$order) {
         </div>
         <div class="order-details">
             <h2 class="order-details__title">Order #<?php echo $order->get_id(); ?></h2>
+            <div class="success-details__item">
+                <div class="success-details__column">
+                    Image
+                </div>
+                <div class="success-details__column">
+                    Name
+                </div>
+                <div class="success-details__column">
+                    Quantity
+                </div>
+                <div class="success-details__column">
+                    <span>Price</span>
+                </div>
+            </div>
             <?php foreach ($order->get_items() as $item): ?>
                 <?php
                     $product = $item->get_product();
                 ?>
                 <div class="success-details__item">
-                    <?php echo $product->get_image(); ?>
-                    <h4 class="success-details__name">
-                        <?php echo $item->get_name(); ?>
-                    </h4>
-                    <p class="success-details__quantity">
-                        Qty: <?php echo $item->get_quantity(); ?>
-                    </p>
-                    <span class="success-details__price">
-                        <?php echo wc_price(
-                            $item->get_total()
-                        ); ?>
-                    </span>
+                    <div class="success-details__column">
+                        <?php echo $product->get_image(); ?>
+                    </div>
+                    <div class="success-details__column">
+                        <h4 class="success-details__name">
+                            <?php echo $item->get_name(); ?>
+                        </h4>
+                    </div>
+                    <div class="success-details__column">
+                        <p class="success-details__quantity">
+                            <?php echo $item->get_quantity(); ?>
+                        </p>
+                    </div>
+                    <div class="success-details__column">
+                        <span class="success-details__price">
+                            <?php echo wc_price(
+                                $item->get_total()
+                            ); ?>
+                        </span>
+                    </div>
                 </div>
             <?php endforeach; ?>
-            <?php echo $order->get_formatted_order_total(); ?>
-            <div class="success-details__message">
-                <h3>
-                    Your order has been placed successfully
-                </h3>
-                <p>
-                    Thank you for your purchase.
-                    We are processing your order.
-                </p>
+            <div class="success-details__item">
+                <span>Total:</span>
+                <span><?php echo $order->get_formatted_order_total(); ?></span>
             </div>
+        </div>
+        <div class="success-details__message">
+            <h3>
+                Your order has been placed successfully
+            </h3>
+            <p>
+                Thank you for your purchase.
+                We are processing your order.
+            </p>
         </div>
     </div>
 </div>
